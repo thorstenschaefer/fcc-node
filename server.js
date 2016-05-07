fb0c8a62d567b74867855a8ac930959c6e184501
Thorsten Schaefer <thorstenschaefer@outlook.com>
2016-05-07 22:16:39 +0000

    Moved service URL to root

--- server.js
+++ server.js
@@ -3,41 +3,17 @@
 var format = require("dateformat");
 var express = require('express');
 
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Bind base URL to a notification that the microservice needs to have a parameter.
-app.get("/timestamp", function(req, res) {
+app.get("/", function(req, res) {
     return res.send('Please provide a date in unix or textual format as a parameter');
 });
 
 // Bind the microservice
-app.get("/timestamp/:dateString", function(req, res) {
+app.get("/:dateString", function(req, res) {
     res.send(createDateObject(getDate(req.params.dateString)));
 });
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
